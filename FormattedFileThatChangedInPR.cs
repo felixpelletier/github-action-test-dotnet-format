dcc939fc711afa5dccf4f10cfe0520d018d8b3d9
Félix Pelletier <felixpelletier23@gmail.com>
2021-12-12 15:42:12 -0500

    Changed some files

--- FormattedFileThatChangedInPR.cs
+++ FormattedFileThatChangedInPR.cs
@@ -6,6 +6,7 @@ namespace FormattingTest
     {
         public static bool FeelsLikeSayingHello()
         {
+            // The program is very friendly.
             return true;
         }
     }
